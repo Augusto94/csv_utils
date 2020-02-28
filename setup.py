a05f5cf0b7240a60d467e1a421b29a225b370adc
augusto94 <arl4@cin.ufpe.br>
2020-02-28 17:09:50 -0300

    [master] Cria arquivo de setup da lib

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='csv_utils',
+    version='0.0.1',
+    url='https://github.com/Augusto94/csv_utils',
+    license='MIT',
+    author='Augusto Lima',
+    author_email='arl4@cin.ufpe.br',
+    description='Python and CSV as never',
+    keywords=['python', 'csv', 'reader', 'writer'],
+    packages=find_packages(),
+    zip_safe=False
+)
